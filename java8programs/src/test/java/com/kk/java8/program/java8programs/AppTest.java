e2a6344134fc74017b39693e1ee263ad651bc8f9
Suthija <kalanghiyakumar.p@gmail.com>
2020-03-26 15:53:43 +0530

    Java 8 Programming Maven basic structure.

---
+++ java8programs/src/test/java/com/kk/java8/program/java8programs/AppTest.java
@@ -0,0 +1,20 @@
+package com.kk.java8.program.java8programs;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
