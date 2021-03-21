0991fb5726b52ce871e94a358615a7d0df445f21
Suthija <kalanghiyakumar.p@gmail.com>
2021-03-21 11:06:53 +0530

    Added basic Spring project structure

---
+++ springbasicexample/src/main/java/com/kk/spring/springbasicexample/App.java
@@ -0,0 +1,13 @@
+package com.kk.spring.springbasicexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
